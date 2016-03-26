d5d806d83eba3e3598619a17ab844374460168c0
Mohamed Alsadek <mohamedalsadek@Mohameds-MacBook-Pro.local>
2016-03-26 19:32:19 +0200

    initial commit

---
+++ FlickrParty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrParty
+//
+//  Created by Mohamed Alsadek on 3/26/16.
+//  Copyright © 2016 Mohamed Alsadek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrParty/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrParty
+//
+//  Created by Mohamed Alsadek on 3/26/16.
+//  Copyright © 2016 Mohamed Alsadek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
